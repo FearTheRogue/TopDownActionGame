12fe4c62384128e6967cff94416ef917f51cddf2
James Bridgman <43548013+FearTheRogue@users.noreply.github.com>
2026-02-04 16:29:41 +0000

    feat: enemy rotation speed
    - Enemy rotation is dampened slightly

--- TopDownActionGame/Assets/Scripts/EnemyFacing.cs
+++ TopDownActionGame/Assets/Scripts/EnemyFacing.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class EnemyFacing : MonoBehaviour
 {
     [SerializeField] private bool rotate = true;
+    [SerializeField] private float rotationSpeed;
 
     public void FaceDirection(Vector2 direction)
     {
@@ -10,7 +11,7 @@ public class EnemyFacing : MonoBehaviour
         if (direction.sqrMagnitude < 0.001f) return;
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-        transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, 0f, angle), Time.deltaTime * rotationSpeed);
     }
 
     public void FaceTarget(Vector2 targetPosition)
