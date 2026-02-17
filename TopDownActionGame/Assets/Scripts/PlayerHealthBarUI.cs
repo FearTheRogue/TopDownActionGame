fbab53cfa19ca58818b735bc7df151c6d404f12c
James Bridgman <43548013+FearTheRogue@users.noreply.github.com>
2026-02-17 11:45:16 +0000

    feat: add dark souls style chip health bar with colour easing on damage

--- TopDownActionGame/Assets/Scripts/PlayerHealthBarUI.cs
+++ TopDownActionGame/Assets/Scripts/PlayerHealthBarUI.cs
@@ -1,22 +1,33 @@
 using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class PlayerHealthBarUI : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] private PlayerHealth playerHealth;
-    [SerializeField] private Image fillImage;
+    [SerializeField] private Image mainFill;
+    [SerializeField] private Image chipFill;
 
     [Header("Animation")]
-    [SerializeField] private float fillSpeed = 8f;
+    [SerializeField] private float chipDelay = 0.25f;
+    [SerializeField] private float chipSpeed = 3f;
+    [SerializeField] private float colourFlashSpeed = 8f;
 
     private float targetFill = 1f;
+    private float chipTargetFill = 1f;
+    private float chipDelayTimer;
+
+    private Color baseColour;
+    private Color flashColour = new Color(1f, 0.3f, 0.3f);
 
     private void Awake()
     {
         if (playerHealth == null)
             playerHealth = FindFirstObjectByType<PlayerHealth>();
+
+        baseColour = mainFill.color;
     }
 
     private void OnEnable()
@@ -37,28 +48,58 @@ public class PlayerHealthBarUI : MonoBehaviour
         if (playerHealth != null)
         {
             targetFill = Mathf.Clamp01((float)playerHealth.CurrentHealth / playerHealth.MaxHealth);
-            
-            if (fillImage != null)
-                fillImage.fillAmount = targetFill;
+
+            if (mainFill != null)
+                mainFill.fillAmount = targetFill;
         }
     }
 
     private void Update()
     {
-        if (fillImage == null)
-            return;
+        // MAIN BAR - instant
+        mainFill.fillAmount = targetFill;
 
-        fillImage.fillAmount = Mathf.Lerp(fillImage.fillAmount, targetFill, Time.deltaTime * fillSpeed);
+        // CHIP BAR - delayed ease
+        if (chipFill.fillAmount > chipTargetFill)
+        {
+            if (chipDelayTimer > 0f)
+            {
+                chipDelayTimer -= Time.deltaTime;
+            }
+            else
+            {
+                chipFill.fillAmount = Mathf.Lerp(chipFill.fillAmount, chipTargetFill, Time.deltaTime * chipSpeed);
+            }
+        }
+        else
+        {
+            chipFill.fillAmount = chipTargetFill;
+        }
 
-        if (Mathf.Abs(fillImage.fillAmount - targetFill) < 0.001f)
-            fillImage.fillAmount = targetFill;
+        // Smooth colour return
+        mainFill.color = Color.Lerp(mainFill.color, baseColour, Time.deltaTime * colourFlashSpeed);
     }
 
     private void HandleHealthChanged(int current, int max)
     {
-        if (max <= 0)
-            return;
+        float newFill = Mathf.Clamp01((float)current / max);
 
-        targetFill = Mathf.Clamp01((float)current / max);
+        if (newFill < targetFill)
+        {
+            // Took Damage
+            chipTargetFill = newFill;
+            chipDelayTimer = chipDelay;
+
+            // Flash colour
+            mainFill.color = flashColour;
+        }
+        else
+        {
+            // Healed
+            chipTargetFill= newFill;
+            chipFill.fillAmount = newFill;
+        }
+
+        targetFill = newFill;
     }
 }
